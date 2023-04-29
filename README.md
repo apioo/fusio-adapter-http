9e7e89704c1f541bb0bab62735b87ae88846d1a5
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-04-29 20:35:33 +0200

    update

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Fusio-Adapter-Http
 install the adapter with the following steps inside your Fusio project:
 
     composer require fusio/adapter-http
-    php bin/fusio system:register Fusio\Adapter\Http\Adapter
+    php bin/fusio system:register "Fusio\Adapter\Http\Adapter"
 
-[Fusio]: http://demo.fusio-project.org/
+[Fusio]: https://www.fusio-project.org/
---
+++ resources/container.php
@@ -0,0 +1,16 @@
+<?php
+
+use Fusio\Adapter\Http\Action\HttpComposition;
+use Fusio\Adapter\Http\Action\HttpLoadBalancer;
+use Fusio\Adapter\Http\Action\HttpProcessor;
+use Fusio\Adapter\Http\Connection\Http;
+use Fusio\Engine\Adapter\ServiceBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $container) {
+    $services = ServiceBuilder::build($container);
+    $services->set(Http::class);
+    $services->set(HttpComposition::class);
+    $services->set(HttpLoadBalancer::class);
+    $services->set(HttpProcessor::class);
+};
